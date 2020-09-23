4b4fb16f0e5ff6f2b6ec53e28f850581a8da3d20
Web Dev <tom2kota@gmail.com>
2020-09-24 00:00:01 +0300

    Add Stripe online service integration

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -3,6 +3,7 @@ import {createStructuredSelector} from "reselect";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cartSelectors";
 import {connect} from "react-redux";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+import {StripeCheckoutButton} from "../../components/stripe-button/StripeButton";
 import './CheckoutPage.scss'
 
 const CheckoutPage = ({cartItems, total}) => (
@@ -31,7 +32,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>Total: ${total}</span>
         </div>
-
+        <div className='warning'>
+            <h2>&#9888;  Use the following credit cards for payments !</h2>
+            <p>4242 4242 4242 4242 (Visa), CVC - Any 3 digits, Date - Any future date </p>
+            <p>5555 5555 5555 4444 (Mastercard), CVC - Any 3 digits, Date - Any future date</p>
+        </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 )
 
